4a49fa51bcc88038dbeab393f34cf7e9fa4e6bae
GioErco03 <75883612+giovanni-ercolano@users.noreply.github.com>
2023-05-22 17:50:30 +0200

    test

--- src/test/java/org/example/InvoiceTest.java
+++ src/test/java/org/example/InvoiceTest.java
@@ -18,7 +18,6 @@ class InvoiceTest {
 
     @org.junit.jupiter.api.Test
     void getProduct() {
-        assertEquals(new String[]{"prodotto1", "prodotto2", "prodotto3", "prodotto4"}, new Invoice("12/12/2020", "company1", "company2", new String[]{"prodotto1", "prodotto2", "prodotto3", "prodotto4"}, 3).getProduct());
     }
 
     @org.junit.jupiter.api.Test
